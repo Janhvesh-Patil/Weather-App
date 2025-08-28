336464d32ef76fc8b5a401b35ca57f48a615f27f
Janhvesh-Patil <janhveshspatil25@gmail.com>
2025-08-28 21:34:35 +0530

    API testing

--- Script.js
+++ Script.js
@@ -1,5 +1,9 @@
-const Base_URL = "http://api.weatherapi.com/v1/current.json";
+const api_key = "e4d83a03265841e28b9135948252808";
+const base_URL = "http://api.weatherapi.com/v1//current.json";
+const cityLocation = "London";
 
-let promise = fetch(Base_URL);
+const url = `${base_URL}?key=${api_key}&q=${cityLocation}&aqi=no`;
+
+let promise = fetch(url);
 
 console.log(promise);
