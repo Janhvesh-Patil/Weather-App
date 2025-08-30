99d9b02bdf70d62c43a8d2b80b78003a3a82e553
Janhvesh-Patil <janhveshspatil25@gmail.com>
2025-08-30 10:44:07 +0530

    Getting info after clicking button

--- Script.js
+++ Script.js
@@ -1,9 +1,34 @@
 const api_key = "e4d83a03265841e28b9135948252808";
-const base_URL = "http://api.weatherapi.com/v1//current.json";
-const cityLocation = "London";
+const base_URL = "https://api.weatherapi.com/v1/current.json";
 
-const url = `${base_URL}?key=${api_key}&q=${cityLocation}&aqi=no`;
+const getWeatherBtn = document.getElementById("Get-weather");
 
-let promise = fetch(url);
+getWeatherBtn.addEventListener("click", () => {
+    const cityLocation = document.getElementById("City-search").value;
+    const url = `${base_URL}?key=${api_key}&q=${cityLocation}&aqi=no`;
 
-console.log(promise);
+    let promise = async () => {
+    let response = await fetch(url);
+    console.log(response);
+
+    let data = await response.json();
+    console.log("Lattitude : ", data.location.lat);
+    console.log("Longitutde : ", data.location.lon);
+    }
+
+    promise();
+})
+
+// const cityLocation = "London";
+
+// const url = `${base_URL}?key=${api_key}&q=${cityLocation}&aqi=no`;
+
+// let promise = async () => {
+//     let response = await fetch(url);
+//     console.log(response);
+
+//     let data = await response.json();
+//     console.log(data.location.lat);
+// }
+
+// promise();
