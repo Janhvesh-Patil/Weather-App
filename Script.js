c84a5d60f4093b884978745db0dfaebe63dadb28
Janhvesh-Patil <janhveshspatil25@gmail.com>
2025-08-31 14:27:22 +0530

    Data display

--- Script.js
+++ Script.js
@@ -10,7 +10,8 @@ getWeatherBtn.addEventListener("click", () => {
     let promise = async () => {
     let response = await fetch(url);
     console.log(response);
-
+    
+    // Respones
     let data = await response.json();
     console.log("Lattitude -> ", data.location.lat);
     console.log("Longitutde -> ", data.location.lon);
@@ -19,23 +20,21 @@ getWeatherBtn.addEventListener("click", () => {
     console.log("Temperature (F) -> ", data.current.temp_f);
     console.log("Humidity : ", data.current.humidity);
     console.log("Wind Data -> [ 'Wind speed(mph)' : ", data.current.wind_mph, ", 'Wind speed(kph)' : ", data.current.wind_kph, " , 'Wind Degree' : ", data.current.wind_degree, " , 'Wind Direction' : ", data.current.wind_dir, " ]");
-
     
-    }
+    //Location data
+    document.getElementById("Location-info").innerHTML = `Lattitude : ${data.location.lat} <br> Longitude : ${data.location.lon}`;
+    
+    //Temperature data
+    document.getElementById("Temp-info").innerHTML = `Celcius : ${data.current.temp_c} °C <br> Fahrenheit : ${data.current.temp_f} °F`;
+    
+    //Weather data
+    document.getElementById("Weather-info").innerHTML = `${data.current.condition.text}`;
 
+    //Humidity data
+    document.getElementById("Humidity-data").innerHTML = `${data.current.humidity} %`;
+
+    //Wind Data
+    document.getElementById("Wind-data").innerHTML = `Wind speed (mph) : ${data.current.wind_mph} mph <br> Wind speed (kmph) : ${data.current.wind_kph} kmph <br> Wind Degree : ${data.current.wind_degree} ° <br> Wind Direction : ${data.current.wind_dir}`;
+    }
     promise();
 })
-
-// const cityLocation = "London";
-
-// const url = `${base_URL}?key=${api_key}&q=${cityLocation}&aqi=no`;
-
-// let promise = async () => {
-//     let response = await fetch(url);
-//     console.log(response);
-
-//     let data = await response.json();
-//     console.log(data.location.lat);
-// }
-
-// promise();
