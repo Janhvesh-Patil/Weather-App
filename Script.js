d365219f36254928dac62244f0b78b07f9ee489f
Janhvesh-Patil <janhveshspatil25@gmail.com>
2025-09-06 22:02:28 +0530

    Removed borders

--- Script.js
+++ Script.js
@@ -1,8 +1,9 @@
+// Weather API data
 const api_key = "e4d83a03265841e28b9135948252808";
 const base_URL = "https://api.weatherapi.com/v1/current.json";
-
 const getWeatherBtn = document.getElementById("Get-weather");
 
+// Fetch weather data
 async function fetchWeather(cityLocation) {
     const url = `${base_URL}?key=${api_key}&q=${cityLocation}&aqi=no`;
     let response = await fetch(url);
@@ -38,42 +39,5 @@ getWeatherBtn.addEventListener("click", () => {
 // On page load, show default city info
 window.addEventListener("DOMContentLoaded", () => {
     const defaultCity = "Pune";
-    fetchWeather(defaultCity);
-});
-// getWeatherBtn.addEventListener("click", () => {
-//     const cityLocation = document.getElementById("City-search").value;
-//     const url = `${base_URL}?key=${api_key}&q=${cityLocation}&aqi=no`;
-
-//     let promise = async () => {
-//     let response = await fetch(url);
-    
-//     // Response
-//     let data = await response.json();
-
-//     const icon_url = data.current.condition.icon;
-
-//     //Weather Icon
-//     document.getElementById("Weather-icon").innerHTML = `<span class="material-symbols-outlined"><img src="${icon_url}" alt="Weather Icon" class="weather-img"></span>`;
-        
-//     //Location data
-//     document.getElementById("Location-info").innerHTML = `Lattitude : ${data.location.lat} <br> Longitude : ${data.location.lon}`;
-    
-//     //Temperature data
-//     document.getElementById("Temp-info").innerHTML = `Celcius : ${data.current.temp_c} °C <br> Fahrenheit : ${data.current.temp_f} °F`;
-    
-//     //Weather data
-//     document.getElementById("Weather-info").innerHTML = `${data.current.condition.text}`;
-
-//     //Humidity data
-//     document.getElementById("Humidity-data").innerHTML = `${data.current.humidity} %`;
-
-//     //Wind Data
-//     document.getElementById("Wind-data").innerHTML = `Wind speed (mph) : ${data.current.wind_mph} <br> Wind speed (kmph) : ${data.current.wind_kph} <br> Wind Degree : ${data.current.wind_degree} ° <br> Wind Direction : ${data.current.wind_dir}`;
-//     }
-//     promise();
-// })
-
-window.addEventListener("DOMContentLoaded", () => {
-    const defaultCity = document.getElementById("City-search").value;
     fetchWeather(defaultCity);
 });
